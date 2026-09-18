e50b23e7ed678d6cc5fa0fce9e4d9550ce98958c
Alexandr-77 <62809713+Alexandr-77@users.noreply.github.com>
2026-09-18 16:47:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// 
+// ================================================== 
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.header__block-nav');
+let liHeader = document.querySelectorAll('.header__menu-item');
+
+burger.addEventListener('click', rotateBurger); 
+
+for(let i = 0; i < liHeader.length; i++ ) {
+	if (window.innerWidth < 990 ) {
+		liHeader[i].addEventListener('click', rotateBurger);
+	}
+}
+
+
+function rotateBurger() { 
+  burger.classList.toggle('burger-active');
+
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
+
+// ===========================================
+// отправка формы =============>
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо. Ваша заявка была отправлена. Наш менеджер свяжется с вами в течение 24 часов.");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
+// ============================
+// ===============================================
+
